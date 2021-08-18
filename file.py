d542cb5f0fb87bd860601a62237de2c6762c043d
Gurminder Singh <83048956+gurminder234@users.noreply.github.com>
2021-08-18 09:25:21 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,15 @@
+
+f = open("practice.txt", "w")
+f.write("Line 1 - A \n" )
+f.write("Line 2 - B \n")
+f.write("Line 3 - C")
+f.close()
+
+
+
+f = open("practice.txt", "r")
+
+
+k = open(
+print(k.read)
+k.close()
